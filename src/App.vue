0d8d2a775f1faa217f99543816911862e5d1a967
Peter Sterckx <pasterckx@gmail.com>
2022-05-27 19:09:18 -0400

    Fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,122 @@
 
 <template>
-  <h1 class="my-6 text-2xl md:text-4xl flex justify-center italic">
-    Tic Tac Toe
-    {{ connectionState }} |
-    {{ queueState }}
-  </h1>
-  <div class="flex justify-center -ml-12">
-    <div
-      class="rounded-full h-6 w-6 mr-6 mt-4 self-start"
-      :class="{
-        'bg-orange-500 animate-pulse': isConnection('CONNECTING'),
-        'bg-green-500': isConnection('ONLINE'),
-        'bg-gray-500': isConnection('OFFLINE'),
-        'animate-pulse': isConnection(['ONLINE', 'CONNECTING']) && isQueue(['WAITING_FOR_OPPONENT', 'REQUESTING_GAME']),
-      }"
-    ></div>
-    <GameBoard
-      :enabled="thisPlayersTurn"
-      @markSquare="markSquare"
-      :board="game.state.board"
-      :marker="game.marker"
-    />
-  </div>
-  <div class="flex justify-center mt-10 flex-wrap mx-10">
-    <button
-      @click="connect()"
-      :class="{
-        'opacity-50': isConnection('ONLINE'),
-        'hover:-translate-y-1': isConnection('OFFLINE'),
-      }"
-      class="bg-green-300 rounded-md p-2 mb-4 mx-2 transition-transform"
-      :disabled="isConnection('ONLINE')"
-    >
-      Start game
-    </button>
-
-    <button
-      @click="disconnect()"
-      :class="{
-        'opacity-50': isConnection('OFFLINE'),
-        'hover:-translate-y-1': isConnection('ONLINE'),
-      }"
-      class="bg-red-300 rounded-md p-2 mb-4 mx-2 transition-transform"
-      :disabled="isConnection('OFFLINE')"
-    >
-      Leave game
-    </button>
-    <button
-      @click="requestGame()"
-      :class="{
-        'opacity-50': !(isConnection('ONLINE') && isGameOver),
-        'hover:-translate-y-1': isConnection('ONLINE') && isGameOver,
-      }"
-      class="bg-blue-300 rounded-md p-2 mb-4 mx-2 transition-transform"
-      :disabled="!(isGameOver || opponentDisconnected)"
-    >
-      Play again
-    </button>
-  </div>
-  <div class="flex flex-col justify-start space-y-1 h-28">
-    <div class="flex justify-center" v-show="game.gameId">
-      You are {{ game.marker }}.
-    </div>
-    <div class="flex justify-center" v-if="game.gameId && !isGameOver">
-      {{ turnMessage }}
-    </div>
-    <div class="flex justify-center" v-show="opponentDisconnected">
-      Your opponent has left the game.
-    </div>
-    <div class="flex justify-center" v-if="isGameOver">
-      {{ gameOverMessage }}
-    </div>
-    <div class="flex justify-center" v-if="isConnection('ONLINE') && isQueue('REQUESTING_GAME')">
-      Requesting game...
-    </div>
-    <div class="flex justify-center" v-if="isQueue('WAITING_FOR_OPPONENT')">
-      Searching for an opponent...
-    </div>
-    <div class="flex justify-center" v-if="isConnection('CONNECTING')">
-      Connecting...
-    </div>
-  </div>
-  <footer
-    class="
-      flex flex-col
-      justify-center
-      align-middle
-      text-center
-      bg-gray-300
-      text-gray-700
-      py-6
-      space-y-1
-    "
-  >
+  <div class="flex flex-col justify-between h-screen bb">
     <div>
-      Built with VueJS and AWS by
-      <a
-        class="font-medium hover:text-gray-900"
-        href="https://petersterckx.com"
-        target="_blank"
-        >Peter Sterckx</a
-      >
+      <h1 class="my-6 text-2xl md:text-4xl flex justify-center italic">
+        Tic Tac Toe
+      </h1>
+      <div class="flex justify-center -ml-12">
+        <div
+          class="rounded-full h-6 w-6 mr-6 mt-4 self-start"
+          :class="{
+            'bg-orange-500 animate-pulse': isConnection('CONNECTING'),
+            'bg-green-500': isConnection('ONLINE'),
+            'bg-gray-500': isConnection('OFFLINE'),
+            'animate-pulse':
+              isConnection(['ONLINE', 'CONNECTING']) &&
+              isQueue(['WAITING_FOR_OPPONENT', 'REQUESTING_GAME']),
+          }"
+        ></div>
+        <GameBoard
+          :enabled="thisPlayersTurn"
+          @markSquare="markSquare"
+          :board="game.state.board"
+          :marker="game.marker"
+        />
+      </div>
+      <div class="flex justify-center mt-10 flex-wrap mx-10">
+        <button
+          @click="connect()"
+          :class="{
+            'opacity-50': isConnection('ONLINE'),
+            'hover:-translate-y-1': isConnection('OFFLINE'),
+          }"
+          class="bg-green-300 rounded-md p-2 mb-4 mx-2 transition-transform"
+          :disabled="isConnection('ONLINE')"
+        >
+          Start game
+        </button>
+
+        <button
+          @click="disconnect()"
+          :class="{
+            'opacity-50': isConnection('OFFLINE'),
+            'hover:-translate-y-1': isConnection('ONLINE'),
+          }"
+          class="bg-red-300 rounded-md p-2 mb-4 mx-2 transition-transform"
+          :disabled="isConnection('OFFLINE')"
+        >
+          Leave game
+        </button>
+        <button
+          @click="requestGame()"
+          :class="{
+            'opacity-50': !(isConnection('ONLINE') && isGameOver),
+            'hover:-translate-y-1': isConnection('ONLINE') && isGameOver,
+          }"
+          class="bg-blue-300 rounded-md p-2 mb-4 mx-2 transition-transform"
+          :disabled="!(isGameOver || opponentDisconnected)"
+        >
+          Play again
+        </button>
+      </div>
+      <div class="flex flex-col justify-start space-y-1 h-28">
+        <div class="flex justify-center" v-show="game.gameId">
+          You are {{ game.marker }}.
+        </div>
+        <div class="flex justify-center" v-if="game.gameId && !isGameOver">
+          {{ turnMessage }}
+        </div>
+        <div class="flex justify-center" v-show="opponentDisconnected">
+          Your opponent has left the game.
+        </div>
+        <div class="flex justify-center" v-if="isGameOver">
+          {{ gameOverMessage }}
+        </div>
+        <div
+          class="flex justify-center"
+          v-if="isConnection('ONLINE') && isQueue('REQUESTING_GAME')"
+        >
+          Requesting game...
+        </div>
+        <div class="flex justify-center" v-if="isQueue('WAITING_FOR_OPPONENT')">
+          Searching for an opponent...
+        </div>
+        <div class="flex justify-center" v-if="isConnection('CONNECTING')">
+          Connecting...
+        </div>
+      </div>
     </div>
-    <a
-      href=""
-      class="flex text-center justify-center space-x-2 hover:text-gray-900"
+    <footer
+      class="
+        flex flex-col
+        justify-center
+        align-middle
+        text-center
+        bg-gray-300
+        text-gray-700
+        py-6
+        space-y-1
+      "
     >
-      <GitHubIcon />
-      <div>Source code</div>
-    </a>
-  </footer>
+      <div>
+        Built with VueJS and AWS by
+        <a
+          class="font-medium hover:text-gray-900"
+          href="https://petersterckx.com"
+          target="_blank"
+          >Peter Sterckx</a
+        >
+      </div>
+      <a
+        href=""
+        class="flex text-center justify-center space-x-2 hover:text-gray-900"
+      >
+        <GitHubIcon />
+        <div>Source code</div>
+      </a>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -240,14 +247,12 @@ export default {
       if (typeof state === "string") {
         return this.connectionState === state;
       }
-      console.log(state.includes(this.connectionState));
       return state.includes(this.connectionState);
     },
     isQueue(state: string | string[]): boolean {
       if (typeof state === "string") {
         return this.queueState === state;
       }
-      console.log(state.includes(this.queueState));
       return state.includes(this.queueState);
     },
     resetGame() {
